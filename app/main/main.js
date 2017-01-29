14f3192676675b846ee4073e774de591bea481f1
Eryk Rakowski <sentialx@gmail.com>
2017-01-29 17:13:02 +0100

    initial commit

---
+++ app/main/main.js
@@ -0,0 +1,42 @@
+var {app, BrowserWindow} = require('electron'),
+    protocol = require('electron').protocol,
+    remote = require('electron').remote,
+    path = require('path');
+
+let mainWindow;
+
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 900, height: 700, frame: false, minWidth: 300, minHeight: 430});
+    mainWindow.loadURL('file://' + __dirname + '/../app/renderer/public/index.html');
+    mainWindow.setMenu(null);
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+    mainWindow.on('unresponsive', function () {
+
+    });
+
+    if (process.env.ENV == 'dev') {
+        mainWindow.webContents.openDevTools();
+    }
+}
+process.on('uncaughtException', function () {
+
+});
+
+app.on('ready', function () {
+    createWindow();
+});
---
+++ app/renderer/components/app.js
@@ -0,0 +1,20 @@
+'use babel';
+import React from 'react';
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                Hello world!
+            </div>
+        );
+    }
+}
